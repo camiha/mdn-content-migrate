4dfeeaf8afff02cca5cd4e45febf893326eb10ee
cyamy <hi@cyamy.dev>
2022-07-09 00:22:25 +0900

    WIP: create functions

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,12 @@
-import { add, mul } from './index';
+import {
+  add,
+  mul,
+  removePElement,
+  replaceCodeElement,
+  replaceStrongElement,
+} from './index';
+
+const log = console.log.bind(console);
 
 describe('sample test describe', () => {
   test('sample test - add', () => {
@@ -7,5 +15,32 @@ describe('sample test describe', () => {
 
   test('sample test - mul', () => {
     expect(mul(1)(2)).toBe(2);
+  });
+
+  test('remove p element', () => {
+    const input = '<p>{{APIRef("Fetch API")}}</p>';
+    const expectResult = '{{APIRef("Fetch API")}}';
+
+    const result = removePElement(input);
+
+    expect(result).toBe(expectResult);
+  });
+
+  test('replace strong element', () => {
+    const input = '<strong>fetch()</strong>';
+    const expectResult = '**fetch()**';
+
+    const result = replaceStrongElement(input);
+
+    expect(result).toBe(expectResult);
+  });
+
+  test('replace strong element', () => {
+    const input = '<code>fetch()</code>';
+    const expectResult = '`fetch()`';
+
+    const result = replaceCodeElement(input);
+
+    expect(result).toBe(expectResult);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,36 @@ export const mul =
   (x: number) =>
   (y: number): number =>
     x * y;
+
+export const removePElement = (input: string): string => {
+  const startElement = '<p>';
+  const endElement = '</p>';
+
+  const result = input.replaceAll(startElement, '').replaceAll(endElement, '');
+
+  return result;
+};
+
+export const replaceStrongElement = (input: string): string => {
+  const startElement = '<strong>';
+  const endElement = '</strong>';
+  const newStr = '**';
+
+  const result = input
+    .replaceAll(startElement, newStr)
+    .replaceAll(endElement, newStr);
+
+  return result;
+};
+
+export const replaceCodeElement = (input: string): string => {
+  const startElement = '<code>';
+  const endElement = '</code>';
+  const newStr = '`';
+
+  const result = input
+    .replaceAll(startElement, newStr)
+    .replaceAll(endElement, newStr);
+
+  return result;
+};
